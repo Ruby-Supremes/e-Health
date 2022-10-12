6cd29ef2cd9c9558adeea4ffcd2e39fa90891496
Jackline Jebet <jebetjackline2016@gmail.com>
2022-10-12 13:45:35 +0300

    added sessions

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,8 +1,4 @@
 class Doctor < ApplicationRecord
-<<<<<<< HEAD
-  has_many :appointments
-  has_many :patients, through: :appointments
-=======
     has_many :appointments, dependent: :destroy
     has_many :patients, through: :appointments
 
@@ -11,5 +7,4 @@ class Doctor < ApplicationRecord
     validates :experience, presence: true
     validates :image, presence: true
 
->>>>>>> 0a6d96889df738ca241b18b51fe34f2d41727765
 end
