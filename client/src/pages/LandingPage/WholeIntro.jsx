d17f1f216dd590c013236429924f891e23745d69
Jobsidney <jobsidney67@gmail.com>
2022-10-12 09:51:57 +0300

    Facts section

--- client/src/pages/LandingPage/WholeIntro.jsx
+++ client/src/pages/LandingPage/WholeIntro.jsx
@@ -7,6 +7,24 @@ function WholeIntro() {
     <NavBar/>
      <div className='motto'>
         <h1>Over a long period of work we have provided hundreds of thousands of health care services</h1>
+        <div className="facts">
+          <h2>Facts</h2>
+          <div className="two-col">
+            <p>
+              Premium equipment that meets the state-of-the-art in the clinical market
+            </p>
+            <p>
+              Professionals in their field, who can be trusted and get the best results
+            </p>
+          </div>
+          <div className="appointment">
+            <h3>Take the sicount for the visit of a doctor</h3>
+            <div className="appoint-btn">
+              <span>20%</span>
+              <button>Make an appointment</button>
+            </div>
+          </div>
+        </div>
      </div>
     </>
   )
