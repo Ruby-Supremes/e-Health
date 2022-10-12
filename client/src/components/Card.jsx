b4e77a217a5647ee2ff4370bf6e23fed7ff19169
Steven Otianga <steveotianga8991@gmail.com>
2022-10-12 15:01:09 +0300

    Added an icon in the card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,16 @@
+import { FaUserAlt } from "react-icons/fa";
+
 const Card = ({ name, occupation }) => {
   return (
-    <div className="card bg-primary mb-5">
-      <div className="card-body">
-        <h5>{name}</h5>
-        <p>{occupation}</p>
+    <div className="card bg-primary mb-5 cardTitle">
+      <div className="card-body d-flex">
+        <span style={{ fontSize: "3rem", color: "white", marginRight:"30px" }}>
+          <FaUserAlt />
+        </span>
+        <div className="d-flex flex-column">
+          <h2 style={{color:"#fff"}}>Name: {name}</h2>
+          <h4>Occupation: {occupation}</h4>
+        </div>
       </div>
     </div>
   );
