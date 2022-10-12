1d43b4a731dfe16d7224485faed1437bc0587a4c
Steven Otianga <steveotianga8991@gmail.com>
2022-10-12 06:46:44 +0300

    Added the router functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,57 @@
 import "./App.css";
-import DoctorDashboard from "./pages/DoctorDashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import Patients from "./pages/Patients";
+import Appointments from "./pages/Appointments";
+import Approvals from "./pages/Approvals";
+// import Appointments from "./pages/Appointments";
 
 function App() {
-  return <DoctorDashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/dashboard"
+          element={
+            <div className="d-flex">
+              <Navbar column="col-2" />
+              <Patients column="col-10" />
+            </div>
+          }
+        />
+        <Route
+          path="/patients"
+          element={
+            <div className="d-flex">
+              <Navbar column="col-2" />
+              <Patients column="col-10" />
+            </div>
+          }
+        />
+        <Route
+          path="/appointments"
+          element={
+            <div className="d-flex">
+              <Navbar column="col-2" />
+              <Appointments column="col-2" />
+            </div>
+          }
+        />
+        <Route
+          path="/approvals"
+          element={
+            <div className="d-flex">
+              <Navbar column="col-2" />
+              <Approvals column="col-2" />
+            </div>
+          }
+        />
+        
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-  
