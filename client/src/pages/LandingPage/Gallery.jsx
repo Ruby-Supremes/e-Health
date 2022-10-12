a5bff627d246f18a2792327730442b6bd44a41ce
Jobsidney <jobsidney67@gmail.com>
2022-10-12 15:29:39 +0300

    Images gallery display

--- client/src/pages/LandingPage/Gallery.jsx
+++ client/src/pages/LandingPage/Gallery.jsx
@@ -3,7 +3,8 @@ import ButtonHashTag from '../../components/ButtonHashTag'
 
 function Gallery() {
   return (
-    <div className='gallery'>
+    <div>
+            <div className='gallery'>
       <h3>Gallery</h3>
       <div className='about'>
         <h1>Check out our clinic great gallery</h1>
@@ -20,6 +21,43 @@ function Gallery() {
         <a href="#" class="fa fa-facebook"></a>
         <a href="#" class="fa fa-whatsapp"></a>
       </div>
+    </div>
+    <div className="image-grid">
+        <div className='wide'>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div className='v-stretch'>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div className='big'>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div >
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div className='wide'>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+        <div>
+            <img src="https://media.istockphoto.com/photos/diagnostic-tools-get-a-digital-upgrade-picture-id1300505874?k=20&m=1300505874&s=612x612&w=0&h=Ev_S8Ag5Pf8LiHL-dW-N8PxgftQ4D8KK-5snYu-Bhn0=" alt="" />
+        </div>
+
+    </div>
     </div>
   )
 }
