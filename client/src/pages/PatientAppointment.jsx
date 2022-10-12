4aebda67feacb1da8f9bc5d29cfffcf1b8903a37
Steven Otianga <steveotianga8991@gmail.com>
2022-10-13 00:33:05 +0300

    Added the booking form

--- client/src/pages/PatientAppointment.jsx
+++ client/src/pages/PatientAppointment.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "../components/Button";
 
 const PatientAppointment = () => {
   return (
     <div className="d-flex fillingForm">
       <div className="d-flex patientForm">
         <img
+          className="img-fluid"
           src="https://images.unsplash.com/photo-1622253692010-333f2da6031d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
           alt="A Doctor"
           style={{ width: "450px", borderRadius: "7px" }}
@@ -12,19 +14,19 @@ const PatientAppointment = () => {
         <form>
           <div className="row">
             <div className="form-group col-6">
-              <label>Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-              />
-            </div>
-            <div className="form-group col-6">
               <label>Name</label>
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter Your Name"
+              />
+            </div>
+            <div className="form-group col-6">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
               />
             </div>
           </div>
@@ -44,6 +46,22 @@ const PatientAppointment = () => {
               placeholder="Enter Occupation"
             />
           </div>
+          <div className="form-group">
+            <label>Nationality</label>
+            <input type="text" className="form-control" placeholder="Kenyan" />
+          </div>
+          <div className="form-group">
+            <label>Date of Birth</label>
+          <input type="date" className="form-control"/>
+          </div>
+          <div class="form-group">
+            <label for="allergy">Allergies:</label>
+            <textarea class="form-control" rows="1" id="allergy"></textarea>
+          </div>
+          <Button
+            word="Book Appointment"
+            classN="btn btn-primary appointmentBtn"
+          />
         </form>
       </div>
     </div>
