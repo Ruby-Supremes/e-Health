a1cf6b7075cc386c10bc45e58e31d754ea96c990
Jackline Jebet <jebetjackline2016@gmail.com>
2022-10-13 22:32:23 +0300

    changed authentication to doctors

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_083309) do
   end
 
   create_table "doctors", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "experience"
     t.string "specialization"
     t.string "image"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
