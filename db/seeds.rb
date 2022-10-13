1620c578338953dccc3d5158c06c4b1ad495661f
Jackline Jebet <jebetjackline2016@gmail.com>
2022-10-13 08:56:39 +0300

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,59 +26,54 @@ Doctor.create!([
 puts "seeding users"
 User.create!([
 
-   { name: "Aaran Kibs",
+   { username: "Aaran Kibs",
      nationality: "Korean",
      date_of_birth: "21062000",
      address: "peetford",
      occupation: "teacher",
-     email: "#",
-     gender: "S",
-     allegies: "peanut",
-     age: "22"
+     allergies: "peanut",
+     age: "22",
+     password: "1234"
    },
 
-   { name: "Aaren Yeager",
+   { username: "Aaren Yeager",
      nationality: "Japanese",
      date_of_birth: "06021994",
      address: "langata",
      occupation: "engineer",
-     email: "#",
-     gender: "M",
-     allegies: "none",
-     age: "28"
+     allergies: "none",
+     age: "28",
+     password: "12345"
    },
 
-   { name: "Aaran Kibs",
+   { username: "Aaran Kibs",
      nationality: "Korean",
      date_of_birth: "21062000",
      address: "monrovia lane",
      occupation: "teacher",
-     email: "#",
-     gender: "S",
-     allegies: "peanut",
-     age: "22"
+     allergies: "peanut",
+     age: "22",
+     password: "123456"
    },
 
-   { name: "Hermadine Smith",
+   { username: "Hermadine Smith",
      nationality: "SouthAfrican",
      date_of_birth: "01172001",
      address: "utawala",
      occupation: "data scientist",
-     email: "#",
-     gender: "F",
-     allegies: "kiwi",
-     age: "17"
+     allergies: "kiwi",
+     age: "17",
+     password: "1234567"
    },
 
-   { name: "Melisa Lonte",
+   { username: "Melisa Lonte",
      nationality: "Kenyan",
      date_of_birth: "21062000",
      address: "jamhuri",
      occupation: "teacher",
-     email: "#",
-     gender: "S",
-     allegies: "peanut",
-     age: "22"
+     allergies: "peanut",
+     age: "22",
+     password: "12345678"
    }
 ])
 
