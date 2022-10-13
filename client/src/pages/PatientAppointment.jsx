6c7dd33b8a3aa5cdde946817507a4c85f457741c
Steven Otianga <steveotianga8991@gmail.com>
2022-10-13 21:02:20 +0300

    Added images in the form component

--- client/src/pages/PatientAppointment.jsx
+++ client/src/pages/PatientAppointment.jsx
@@ -11,7 +11,7 @@ const PatientAppointment = () => {
   const [allergies, setAllergies] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     fetch("/users", {
       method: "POST",
       body: JSON.stringify({
@@ -31,12 +31,82 @@ const PatientAppointment = () => {
   return (
     <div className="d-flex fillingForm">
       <div className="d-flex patientForm">
-        <img
-          className="img-fluid"
-          src="https://images.unsplash.com/photo-1622253692010-333f2da6031d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-          alt="A Doctor"
-          style={{ width: "450px", borderRadius: "7px" }}
-        />
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide col-8"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+              className="bg-primary"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img
+                src="https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                className="img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://images.unsplash.com/photo-1537368910025-700350fe46c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                className="img-fluid"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                className="img-fluid"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="form-group col-6">
@@ -87,13 +157,13 @@ const PatientAppointment = () => {
               type="date"
               className="form-control"
               value={date}
-              onChange={e => setDate(e.target.value)}
+              onChange={(e) => setDate(e.target.value)}
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="allergy">Allergies:</label>
             <textarea
-              class="form-control"
+              className="form-control"
               rows="1"
               id="allergy"
               value={allergies}
